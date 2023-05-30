421753c1842643a841425d766cf7e5660ebb2274
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-05-30 15:43:05 +0800

    aw

--- slp-frontend/next.config.js
+++ slp-frontend/next.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    retrieve_projects: "http://localhost:8080/api/v1/project/all",
-    retrieve_mapped_projects: "http://localhost:8080/api/v1/project/mapped_projects",
-    create_project: "http://localhost:8080/api/v1/project/create",
-    delete_project: "http://localhost:8080/api/v1/project/delete/{id}",
-    update_project: "http://localhost:8080/api/v1/project/update/{id}",
+    retrieve_projects: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8081/api/v1/project/all",
+    retrieve_mapped_projects: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8081/api/v1/project/mapped_projects",
+    create_project: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8081/api/v1/project/create",
+    delete_project: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8081/api/v1/project/delete/{id}",
+    update_project: "http://ec2-54-253-215-31.ap-southeast-2.compute.amazonaws.com:8081/api/v1/project/update/{id}",
 
   }
 
